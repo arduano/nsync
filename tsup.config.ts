6dc3275df6d2df61e8922c009a74e1a8b9373807
arduano <leonid.shchurov@gmail.com>
2025-09-25 22:46:56 +1000

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ watch, entry, external }) => {
     bundle: true,
     metafile: true,
     treeshake: true,
-    noExternal: ["cmd-ts", "execa", "zod", "nanoid"],
+    noExternal: ["cmd-ts", "execa", "zod", "nanoid", "signale"],
     loader: {
       ".json": "copy",
     },
